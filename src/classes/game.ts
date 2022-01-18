6998c2658185f2dac665cb95d12b0a70a42b1fd6
sqooid <lucasliu7@hotmail.com>
2022-01-18 17:54:08 +1100

    Added hand modicification helpers

--- src/classes/game.ts
+++ src/classes/game.ts
@@ -1,9 +1,11 @@
 import { Player } from '@/interfaces/player'
-import { Play } from '@/interfaces/play'
+import { Play, validPlay } from '@/interfaces/play'
 import { deal, DealOptions } from '@/utils/deal'
+import { containsCards, removeCards } from '@/interfaces/deck'
 
 interface State {
   turn: number
+  playerTurnIndex: number
   players: Player[]
   board: Play[]
 }
@@ -19,19 +21,51 @@ export class Game {
   history: State[]
 
   constructor() {
-    this.state = { turn: 0, players: [], board: [] }
+    this.clear()
+  }
+
+  /**
+   * Clears all existing state
+   */
+  clear() {
+    this.state = { turn: 1, playerTurnIndex: 0, players: [], board: [] }
     this.history = []
   }
 
+  /**
+   * Deals cards to players as fresh game
+   * @param options Deal Options
+   */
   dealCards(options: DealOptions) {
+    this.clear()
     const hands = deal(options)
     for (const hand of hands) {
       this.state.players.push({ cards: hand })
     }
   }
 
-  makePlay(play: Play) {
-    return
+  /**
+   * Makes play and advances turn if play was valid, no change otherwise
+   * @param play Play to make, with sorted cards
+   * @returns True if play was valid, false if invalid play
+   */
+  makePlay(play: Play): boolean {
+    const player = this.state.players[this.state.playerTurnIndex]
+    if (!containsCards(play.cards, player.cards)) {
+      return false
+    }
+
+    const board = this.state.board
+    const prevPlay = board[board.length - 1]
+    if (validPlay(play, prevPlay)) {
+      removeCards(play.cards, player.cards)
+      board.push(play)
+      this.history.push()
+      ++this.state.turn
+      this.state.playerTurnIndex
+      return true
+    }
+    return false
   }
 }
 
