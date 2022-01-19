18a23b22d3ca846d1c41f8531c6534060855192d
sqooid <lucasliu7@hotmail.com>
2022-01-19 12:45:31 +1100

    Removed history from game

--- src/classes/game.ts
+++ src/classes/game.ts
@@ -3,42 +3,39 @@ import { clonePlay, Play, validPlay } from '@/interfaces/play'
 import { deal, DealOptions } from '@/utils/deal'
 import { containsCards, removeCards } from '@/interfaces/deck'
 
+interface BoardPlay {
+  playerIndex: number
+  play: Play
+}
+
 export interface State {
-  winnerIndex?: number
   turn: number
   turnIndex: number
-  lastPlayIndex: number
   players: Player[]
-  board: Play[]
+  board: BoardPlay[]
 }
 
-/**
- * Clones the state 'deeply enough', assuming cards aren't modified in place
- * @param state
- * @returns Cloned state
- */
-function cloneState(state: State): State {
-  return {
-    winnerIndex: state.winnerIndex,
-    turn: state.turn,
-    turnIndex: state.turnIndex,
-    lastPlayIndex: state.lastPlayIndex,
-    players: state.players.map((player) => {
-      return { cards: [...player.cards] }
-    }),
-    board: [...state.board],
-  }
-}
+// /**
+//  * Clones the state 'deeply enough', assuming cards aren't modified in place
+//  * @param state
+//  * @returns Cloned state
+//  */
+// function cloneState(state: State): State {
+//   return {
+//     winnerIndex: state.winnerIndex,
+//     turn: state.turn,
+//     turnIndex: state.turnIndex,
+//     lastPlayIndex: state.lastPlayIndex,
+//     players: state.players.map((player) => {
+//       return { cards: [...player.cards] }
+//     }),
+//     board: [...state.board],
+//   }
+// }
 
 export class Game {
-  /**
-   * Current state of the game
-   */
+  _finished: boolean
   _state: State
-  /**
-   * Past states
-   */
-  _history: State[]
 
   constructor() {
     this.clear()
@@ -51,11 +48,9 @@ export class Game {
     this._state = {
       turn: 0,
       turnIndex: 0,
-      lastPlayIndex: 0,
       players: [],
       board: [],
     }
-    this._history = []
   }
 
   /**
@@ -77,7 +72,14 @@ export class Game {
    * @returns True if play was valid, false if invalid play
    */
   makePlay(play?: Play): boolean {
-    const player = this._state.players[this._state.turnIndex]
+    if (this._finished) return false
+    if (!play) {
+      this._tickBoard()
+      return true
+    }
+
+    const turnIndex = this._state.turnIndex
+    const player = this._state.players[turnIndex]
     if (!containsCards(play.cards, player.cards)) {
       return false
     }
@@ -85,29 +87,29 @@ export class Game {
     const board = this._state.board
     const prevPlay = board[board.length - 1]
 
-    if (validPlay(play, prevPlay)) {
-      this._history.push(cloneState(this._state))
-
+    if (validPlay(play, prevPlay?.play) || prevPlay.playerIndex === turnIndex) {
+      // Tick state
       removeCards(play.cards, player.cards)
       const playClone = clonePlay(play)
-      board.push(playClone)
+      board.push({ play: playClone, playerIndex: turnIndex })
 
-      ++this._state.turn
-      this._state.turnIndex =
-        (this._state.turnIndex + 1) % this._state.players.length
+      this._tickBoard()
 
       // Winning play
       if (player.cards.length === 0) {
-        this._state.winnerIndex =
-          (this._state.turnIndex - 1) % this._state.players.length
-        // Chuck winning play onto history as well for completeness
-        this._history.push(cloneState(this._state))
+        this._finished = true
       }
 
       return true
     }
 
     return false
+  }
+
+  _tickBoard() {
+    ++this._state.turn
+    this._state.turnIndex =
+      (this._state.turnIndex + 1) % this._state.players.length
   }
 }
 
--- tests/unit/game.spec.ts
+++ tests/unit/game.spec.ts
@@ -1,4 +1,4 @@
-import { createGame, Game } from '@/classes/game'
+import { createGame, Game, State } from '@/classes/game'
 import { newCard, Suit } from '@/interfaces/deck'
 import { findPlay, Hand } from '@/interfaces/play'
 import { expect, util } from 'chai'
@@ -6,7 +6,6 @@ import { utils } from 'mocha'
 import rewire from 'rewire'
 
 const gameRewire = rewire('@/classes/game.ts')
-const cloneState = gameRewire.__get__('cloneState')
 
 describe('game.ts', () => {
   it('Game dealCards 4 players', () => {
@@ -22,86 +21,6 @@ describe('game.ts', () => {
       playerCount: 3,
     })
     expect(game._state.players).to.have.lengthOf(3)
-  })
-  it('cloneState 1 turn', () => {
-    const state = {
-      winnerIndex: -1,
-      turn: 2,
-      turnIndex: 1,
-      players: [
-        {
-          cards: [newCard(Suit.CLUB, 3)],
-        },
-        {
-          cards: [newCard(Suit.HEART, 3)],
-        },
-      ],
-      board: [
-        {
-          combo: Hand.SINGLE,
-          comboValue: newCard(Suit.DIAMOND, 3),
-          cards: [newCard(Suit.DIAMOND, 3)],
-        },
-      ],
-    }
-    const jsonClonedState = JSON.parse(JSON.stringify(state))
-    const clonedState = cloneState(state)
-    state.turn++
-    state.turnIndex++
-    state.players[1].cards.splice(0, 1)
-    const cards = [newCard(Suit.DIAMOND, 3)]
-    state.board.push({
-      combo: Hand.SINGLE,
-      comboValue: cards[0],
-      cards,
-    })
-    expect(clonedState).to.eql(jsonClonedState)
-  })
-  it('cloneState 2 turns', () => {
-    const state = {
-      winnerIndex: -1,
-      turn: 2,
-      turnIndex: 1,
-      players: [
-        {
-          cards: [newCard(Suit.CLUB, 3)],
-        },
-        {
-          cards: [newCard(Suit.HEART, 3)],
-        },
-      ],
-      board: [
-        {
-          combo: Hand.SINGLE,
-          comboValue: newCard(Suit.DIAMOND, 3),
-          cards: [newCard(Suit.DIAMOND, 3)],
-        },
-      ],
-    }
-    const jsonClonedState1 = JSON.parse(JSON.stringify(state))
-    const clonedState1 = cloneState(state)
-    state.turn++
-    state.turnIndex = 0
-    state.players[1].cards.splice(0, 1)
-    const cards1 = [newCard(Suit.DIAMOND, 3)]
-    state.board.push({
-      combo: Hand.SINGLE,
-      comboValue: cards1[0],
-      cards: cards1,
-    })
-    const jsonClonedState2 = JSON.parse(JSON.stringify(state))
-    const clonedState2 = cloneState(state)
-    state.turn++
-    state.turnIndex = 1
-    state.players[0].cards.splice(0, 1)
-    const cards2 = [newCard(Suit.CLUB, 3)]
-    state.board.push({
-      combo: Hand.SINGLE,
-      comboValue: cards2[0],
-      cards: cards2,
-    })
-    expect(clonedState1).to.eql(jsonClonedState1) &&
-      expect(clonedState2).to.eql(jsonClonedState2)
   })
   it('makePlay invalid combo', () => {
     const game = createGame()
@@ -122,9 +41,9 @@ describe('game.ts', () => {
       newCard(Suit.HEART, 10),
       newCard(Suit.DIAMOND, 12),
     ])
-    play.cards[0].value = 10
+    play.cards[4].value = 11
 
-    expect(game.makePlay(play)).to.be.false && expect(state).to.eql(game._state)
+    expect(game.makePlay(play)).to.be.false && expect(game._state).to.eql(state)
   })
   it('makePlay missing cards', () => {
     const game = createGame()
@@ -146,7 +65,34 @@ describe('game.ts', () => {
       newCard(Suit.DIAMOND, 12),
     ])
 
-    expect(game.makePlay(play)).to.be.false && expect(state).to.eql(game._state)
+    expect(game.makePlay(play)).to.be.false && expect(game._state).to.eql(state)
+  })
+  it('makePlay pass', () => {
+    const game = createGame()
+    game._state.players = [
+      {
+        cards: [newCard(Suit.CLUB, 11)],
+      },
+      {
+        cards: [newCard(Suit.CLUB, 11)],
+      },
+      {
+        cards: [newCard(Suit.CLUB, 11)],
+      },
+      {
+        cards: [newCard(Suit.CLUB, 11)],
+      },
+    ]
+    const playersClone = JSON.parse(JSON.stringify(game._state.players))
+    const expectedState: State = {
+      turn: 1,
+      turnIndex: 1,
+      board: [],
+      players: playersClone,
+    }
+
+    expect(game.makePlay()).to.be.true &&
+      expect(game._state).to.eql(expectedState)
   })
   it('makePlay non-winning', () => {
     const game = createGame()
@@ -171,7 +117,6 @@ describe('game.ts', () => {
         cards: [newCard(Suit.CLUB, 11)],
       },
     ]
-    const playersCopy = JSON.parse(JSON.stringify(game._state.players))
     const play = findPlay([
       newCard(Suit.CLUB, 11),
       newCard(Suit.HEART, 1),
@@ -180,36 +125,27 @@ describe('game.ts', () => {
       newCard(Suit.DIAMOND, 12),
     ])
     const res = game.makePlay(play)
-
-    expect(res).to.be.true &&
-      expect(game._state, 'state').to.eql({
-        turn: 1,
-        turnIndex: 1,
-        players: [
-          {
-            cards: [newCard(Suit.DIAMOND, 11)],
-          },
-          {
-            cards: [newCard(Suit.CLUB, 11)],
-          },
-          {
-            cards: [newCard(Suit.CLUB, 11)],
-          },
-          {
-            cards: [newCard(Suit.CLUB, 11)],
-          },
-        ],
-        board: [play],
-      }) &&
-      expect(game._history, 'history').to.eql([
+    const expectedState: State = {
+      turn: 1,
+      turnIndex: 1,
+      board: [{ play, playerIndex: 0 }],
+      players: [
         {
-          winnerIndex: undefined,
-          turn: 0,
-          turnIndex: 0,
-          players: playersCopy,
-          board: [],
+          cards: [newCard(Suit.DIAMOND, 11)],
         },
-      ])
+        {
+          cards: [newCard(Suit.CLUB, 11)],
+        },
+        {
+          cards: [newCard(Suit.CLUB, 11)],
+        },
+        {
+          cards: [newCard(Suit.CLUB, 11)],
+        },
+      ],
+    }
+
+    expect(res).to.be.true && expect(game._state, 'state').to.eql(expectedState)
   })
   it('makePlay winning', () => {
     const game = createGame()
@@ -234,8 +170,6 @@ describe('game.ts', () => {
       },
     ]
 
-    const playersCopy = JSON.parse(JSON.stringify(game._state.players))
-
     const play = findPlay([
       newCard(Suit.CLUB, 11),
       newCard(Suit.HEART, 1),
@@ -244,55 +178,28 @@ describe('game.ts', () => {
       newCard(Suit.DIAMOND, 12),
     ])
 
+    const expectedState: State = {
+      turn: 1,
+      turnIndex: 1,
+      board: [{ playerIndex: 0, play }],
+      players: [
+        {
+          cards: [],
+        },
+        {
+          cards: [newCard(Suit.CLUB, 11)],
+        },
+        {
+          cards: [newCard(Suit.CLUB, 11)],
+        },
+        {
+          cards: [newCard(Suit.CLUB, 11)],
+        },
+      ],
+    }
+
     expect(game.makePlay(play)).to.be.true &&
-      expect(game._state).to.eql({
-        winnerIndex: 0,
-        turn: 1,
-        turnIndex: 1,
-        players: [
-          {
-            cards: [],
-          },
-          {
-            cards: [newCard(Suit.CLUB, 11)],
-          },
-          {
-            cards: [newCard(Suit.CLUB, 11)],
-          },
-          {
-            cards: [newCard(Suit.CLUB, 11)],
-          },
-        ],
-        board: [play],
-      }) &&
-      expect(game._history).to.eql([
-        {
-          winnerIndex: undefined,
-          turn: 0,
-          turnIndex: 0,
-          players: playersCopy,
-          board: [],
-        },
-        {
-          winnerIndex: 0,
-          turn: 1,
-          turnIndex: 1,
-          players: [
-            {
-              cards: [],
-            },
-            {
-              cards: [newCard(Suit.CLUB, 11)],
-            },
-            {
-              cards: [newCard(Suit.CLUB, 11)],
-            },
-            {
-              cards: [newCard(Suit.CLUB, 11)],
-            },
-          ],
-          board: [play],
-        },
-      ])
+      expect(game._state).to.eql(expectedState) &&
+      expect(game.makePlay()).to.be.false
   })
 })
